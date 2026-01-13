0968a6b61b4c5510dc396865618cc72f0de375d3
Abhishek Sonawane <abhisheksonwane57@gmail.com>
2026-01-13 17:02:05 +0530

    fix routing issue

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StatusBar, useColorScheme } from 'react-native';
+import { StatusBar, Text, useColorScheme, View} from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Root from './src/container/root';
+// import { View } from 'react-native/types_generated/index';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -10,6 +11,7 @@ function App() {
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Root />
+   
     </SafeAreaProvider>
   );
 }
