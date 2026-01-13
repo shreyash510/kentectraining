7fb735b9723670582df3d31962a73d898552d831
Abhishek Sonawane <abhisheksonwane57@gmail.com>
2026-01-13 18:44:23 +0530

    drawer added

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
+  /** @type {import('react-native-worklets/plugin').PluginOptions} */
+  const workletsPluginOptions = {
+    // Your custom options.
+  }
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: ['react-native-reanimated/plugin'
+  ],
 };
